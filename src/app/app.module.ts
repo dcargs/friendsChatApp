37c209de0427ab4096d180955a8eb4c42f82b48c
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-12-05 19:10:22 -0600

    first and only commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+// Import the AF2 Module
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AuthProvider } from '../providers/auth/auth';
+
+// Importing AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyABXA9nfawiYhdthe7gkmjjL3r6Ryv7FwA",
+  authDomain: "friends-chat-app-69.firebaseapp.com",
+  databaseURL: "https://friends-chat-app-69.firebaseio.com",
+  projectId: "friends-chat-app-69",
+  storageBucket: "friends-chat-app-69.appspot.com",
+  messagingSenderId: "825015271614"
+};
 
 @NgModule({
   declarations: [
@@ -13,8 +31,11 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    BrowserModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -22,9 +43,10 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
-    StatusBar,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    StatusBar,
+    AuthProvider
   ]
 })
 export class AppModule {}
